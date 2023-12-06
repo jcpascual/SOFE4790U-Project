be2f7fa911362e465edc56496c234e7d4c9e03ad
jcpascual <julian.pascual@ontariotechu.net>
2023-12-05 20:48:06 -0500

    DataStreamExtensions: Add comments

--- src/net/uoit/sofe4790/project/common/util/DataStreamExtensions.java
+++ src/net/uoit/sofe4790/project/common/util/DataStreamExtensions.java
@@ -6,25 +6,36 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class DataStreamExtensions {
+    // Helper method for writing Strings to a DataOutputStream.
     public static void writeUtf8String(DataOutputStream stream, String str) throws IOException {
+        // Get the UTF-8 bytes of the value.
         byte[] data = str.getBytes(StandardCharsets.UTF_8);
+
+        // Write the byte array.
         writeByteArray(stream, data);
     }
 
     public static String readUtf8String(DataInputStream stream) throws IOException {
+        // Get the UTF-8 bytes.
         byte[] data = readByteArray(stream);
+
+        // Construct a String instance using them.
         return new String(data, StandardCharsets.UTF_8);
     }
 
     public static void writeByteArray(DataOutputStream stream, byte[] b) throws IOException {
+        // Write the length of the byte array.
         stream.writeInt(b.length);
+
+        // Write the bytes.
         stream.write(b);
     }
 
     public static byte[] readByteArray(DataInputStream stream) throws IOException {
+        // Read the number of bytes.
         int len = stream.readInt();
-        byte[] data = stream.readNBytes(len);
 
-        return data;
+        // Read each byte into the byte array.
+        return stream.readNBytes(len);
     }
 }
